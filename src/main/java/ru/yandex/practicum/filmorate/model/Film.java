6b7176c1975b60626f61a68569498bf39da2b956
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-04-22 02:07:49 +0300

    fix: сделала плюс два dao

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -14,22 +14,13 @@ import java.util.Set;
 public class Film {
 
     @NonNull
-    long id;
-    String name;
-    String description;
-    LocalDate releaseDate;
-    int duration;
+    final long id;
+    final String name;
+    final String description;
+    final LocalDate releaseDate;
+    final int duration;
     @NonNull
-    Mpa mpa;
+    final Mpa mpa;
     Set<Genre> genres = new LinkedHashSet<>();
-
-    public Film(long id, String name, String description, LocalDate releaseDate, int duration, Mpa mpa) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.releaseDate = releaseDate;
-        this.duration = duration;
-        this.mpa = mpa;
-    }
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,29 +1,23 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
 
+@AllArgsConstructor
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
 
     @NonNull
-    long id;
-    String email;
-    String login;
+    final long id;
+    final String email;
+    final String login;
     String name;
-    LocalDate birthday;
-
-    public User(@NonNull long id, String email, String login, String name, LocalDate birthday) {
-        this.id = id;
-        this.email = email;
-        this.login = login;
-        this.name = name;
-        this.birthday = birthday;
-    }
+    final LocalDate birthday;
 
 }
